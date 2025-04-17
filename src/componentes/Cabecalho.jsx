f5b523a610b3288bbb3606269e2c2721ae05efa3
GiovanaCris <giovana.bp@outlook.com>
2025-04-17 11:35:35 -0300

    projeto

---
+++ src/componentes/Cabecalho.jsx
@@ -0,0 +1,9 @@
+import estilos from './Cabecalho.module.css';
+//Estrutura básica para os componentes
+export function Cabecalho(){ //deve ser o msm nome do arquivo
+    return(
+        <header className={estilos.container}>
+            <h1>Giflix</h1>
+        </header>
+    )
+}
