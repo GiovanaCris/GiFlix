9d61c6e19c15c960549150557de5d1f22fe8ba4c
GiovanaCris <giovana.bp@outlook.com>
2025-04-29 00:48:39 -0300

    ultimo giflix atualizado

---
+++ Giflix atualizado3/src/Paginas/Favoritos.jsx
@@ -0,0 +1,9 @@
+import estilo from './Favoritos.module.css';
+
+export function Favoritos() {
+    return(
+        <main className={estilo.container}>
+            <h2>Meus Favoritos</h2>
+        </main>
+    );
+}
