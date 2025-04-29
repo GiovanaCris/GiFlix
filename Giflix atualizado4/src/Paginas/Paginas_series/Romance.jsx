d8d42e92d30d30799e70d5e4c4acdf6a3934e84d
GiovanaCris <giovana.bp@outlook.com>
2025-04-29 16:56:26 -0300

    atualizado hoje

---
+++ Giflix atualizado4/src/Paginas/Paginas_series/Romance.jsx
@@ -0,0 +1,13 @@
+import estilos from './Romance.module.css';
+import estilo from '../Perfil.module.css';
+
+export function Romance() {
+    return(
+        <main className={estilo.container}>
+            <div className={estilo.textperfil}>
+                <h3>- Séries de Romance para você!</h3>
+            </div>
+        </main>
+    )
+}
+export default Romance; 
