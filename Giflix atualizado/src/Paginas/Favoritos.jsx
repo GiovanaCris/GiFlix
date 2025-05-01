5860d3ff010101f60e08bbb47c2ff76633946000
Giovana <giovana.bp@outlook.com>
2025-04-30 23:16:49 -0300

    Delete Giflix atualizado directory

--- Giflix atualizado/src/Paginas/Favoritos.jsx
+++
@@ -1,9 +0,0 @@
-import estilo from './Favoritos.module.css';
-
-export function Favoritos() {
-    return(
-        <main className={estilo.container}>
-            <h2>Meus Favoritos</h2>
-        </main>
-    );
-}
