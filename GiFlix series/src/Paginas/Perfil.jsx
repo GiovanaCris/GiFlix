8f70a5aca9ddb7194bd5057d66b4450c3daeb482
Giovana <giovana.bp@outlook.com>
2025-04-30 23:16:31 -0300

    Delete GiFlix series directory

--- GiFlix series/src/Paginas/Perfil.jsx
+++
@@ -1,9 +0,0 @@
-import estilo from './Perfil.module.css';
-
-export function Perfil() {
-    return(
-        <main className={estilo.container}>
-            <h2>Meu Perfil</h2>
-        </main>
-    );
-}
--- GiFlix series/src/componentes/Cabecalho.jsx
+++
@@ -1,9 +0,0 @@
-import estilos from './Cabecalho.module.css';
-//Estrutura básica para os componentes
-export function Cabecalho(){ //deve ser o msm nome do arquivo
-    return(
-        <header className={estilos.container}>
-            <h1>Giflix</h1>
-        </header>
-    )
-}
